00c957b05359f0fa02b0fc7ecb3931e77491f84d
Mark-Ameka <xenonmendozaxenon@gmail.com>
2023-12-18 15:43:18 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(props) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">sample</h1>
+    </div>
+  );
+}
+
+export default App;
